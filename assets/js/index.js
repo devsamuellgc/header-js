c49247dbc361e621ef5d4328b72b512037c62801
devsamuellgc <samuelgalrao@hotmail.com>
2023-06-05 09:55:19 -0300

    Formulário com regex, validação

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,3 +19,36 @@ linkDropdown.addEventListener('mouseout', closeLinkDropdown)
 cardDropdown.addEventListener('mouseover', openLinkDropdown)
 cardDropdown.addEventListener('mouseout', closeLinkDropdown)
 locationDropdown.addEventListener('mouseover', openLocationDropdown)
+
+//Formulário
+
+const formulario = document.getElementById('form');
+const license = document.getElementById('license');
+const invalidLicense = document.getElementById('invalid-license');
+const invalidState = document.getElementById('invalid-state');
+const state = document.getElementById('state');
+const zip = document.getElementById('zip');
+const numberPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (license.value.match( numberPattern )) {
+        license.style.border = '2px solid green'
+        invalidLicense.style.display = 'none';
+    } else {
+        license.style.border = '2px solid red'
+        invalidLicense.style.display = 'flex'
+    }
+
+    if (state.value === 'ma') {
+        alert('Estado indisponível')
+    } else if (state.value) {
+        state.style.border = '2px solid green'
+        invalidState.style.display = 'none'
+    } else {
+        state.style.border = '2px solid red'
+        invalidState.style.display = 'flex'
+    }
+
+
+})
